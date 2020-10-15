6cf9db2e0035f6db0dca131b681a2c48128b0df4
Arif016210 <arifulislamwasim2017@gmail.com>
2020-10-15 21:34:55 +0600

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kbwel.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://creativeAgencyProject:wasim123@cluster0.kbwel.mongodb.net/agencyDatabase?retryWrites=true&w=majority";
 
 const app = express();
 app.use(bodyParser.json());
